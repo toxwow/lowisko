f899164623988ffd0dc781256713b5f43b99456c
Tomasz Tomżyński <tomasztomzynski@MacBook-Pro-Tomasz.local>
2019-06-11 22:07:05 +0200

    test

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,5 +1,5 @@
 @extends('errors::minimal')
 
-@section('title', __('Not Found'))
+@section('title', __('Nie znaleziono strony'))
 @section('code', '404')
-@section('message', __('Not Found'))
+@section('message', __('Nie znaleziono strony'))
