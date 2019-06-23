c3e044a0df3a7e0111fb3c9a5e98e5751cd4c156
Tomasz Tomżyński <tomasztomzynski@MacBook-Pro-Tomasz.local>
2019-06-23 12:14:51 +0200

    add googleads

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,6 +1,13 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({
+            google_ad_client: "ca-pub-8794739569727921",
+            enable_page_level_ads: true
+        });
+    </script>
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-80710173-1"></script>
     <script>
